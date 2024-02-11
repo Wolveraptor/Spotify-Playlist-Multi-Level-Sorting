4684c4272a67c3d4437a28fbf8bb006ff257a1a1
Stephen Simon <48845193+Wolveraptor@users.noreply.github.com>
2024-02-11 11:26:45 -0500

    Added comment for required scopes.

--- get_current_users_profile.py
+++ get_current_users_profile.py
@@ -13,6 +13,7 @@ from spotify_developer_application import (spotify_client_id,
 get_current_users_profile = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id,
                                                client_secret=spotify_client_secret,
                                                redirect_uri=spotify_redirect_uri,
+                                               # Scopes required for obtaining current user's profile can be found here: https://developer.spotify.com/documentation/web-api/reference/get-current-users-profile
                                                scope="user-read-private user-read-email"))
 
 # Print user details in console.
