337affcd3d8bf64e6b8109a0367d9b8b3034917d
Stephen Simon <48845193+Wolveraptor@users.noreply.github.com>
2024-02-11 18:06:17 -0500

    Updated code indentation.

--- get_current_users_profile.py
+++ get_current_users_profile.py
@@ -6,15 +6,15 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 # spotify_developer_application is used for passing the spotify_client_id, spotify_client_secret, and spotify_redirect_uri variables.
 from spotify_developer_application import (spotify_client_id,
-                                          spotify_client_secret,
-                                          spotify_redirect_uri)
+                                           spotify_client_secret,
+                                           spotify_redirect_uri)
 
 # Authorization Code Flow to obtain current user's profile.
 get_current_users_profile = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id,
-                                               client_secret=spotify_client_secret,
-                                               redirect_uri=spotify_redirect_uri,
-                                               # Scopes required for obtaining current user's profile can be found here: https://developer.spotify.com/documentation/web-api/reference/get-current-users-profile
-                                               scope="user-read-private user-read-email"))
+                                                                      client_secret=spotify_client_secret,
+                                                                      redirect_uri=spotify_redirect_uri,
+                                                                      # Scopes required for obtaining current user's profile can be found here: https://developer.spotify.com/documentation/web-api/reference/get-current-users-profile
+                                                                      scope="user-read-private user-read-email"))
 
 # Create variable to store data from current_user method of spotipy
 get_current_users_profile_data = get_current_users_profile.current_user()
