39cd522e82c7b99a971a36a249ad29ae6c03e216
Stephen Simon <48845193+Wolveraptor@users.noreply.github.com>
2024-02-11 11:24:16 -0500

    Renamed user_details variable to get_current_users_profile

--- get_current_users_profile.py
+++ get_current_users_profile.py
@@ -10,10 +10,10 @@ from spotify_developer_application import (spotify_client_id,
                                           spotify_redirect_uri)
 
 # Authorization Code Flow to obtain user details.
-user_details = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id,
+get_current_users_profile = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id,
                                                client_secret=spotify_client_secret,
                                                redirect_uri=spotify_redirect_uri,
                                                scope="user-read-private user-read-email"))
 
 # Print user details in console.
-print(json.dumps(user_details.me(), indent=4, sort_keys=True))
+print(json.dumps(get_current_users_profile.me(), indent=4, sort_keys=True))
