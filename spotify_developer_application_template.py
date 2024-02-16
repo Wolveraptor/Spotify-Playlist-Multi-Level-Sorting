0577cd17a16a4170e7dd207cda5efc3fc8588941
Stephen Simon <48845193+Wolveraptor@users.noreply.github.com>
2024-02-16 15:30:15 -0500

    Updated spotify_developer_application_template.py

--- spotify_developer_application_template.py
+++ spotify_developer_application_template.py
@@ -1,4 +1,4 @@
 # The values for the variables "spotify_client_id", "spotify_client_secret", and "spotify_redirect_uri" are gathered from the Spotify Developer Dashboard.
 spotify_application_client_id = "Client ID from Spotify Developer Dashboard"
 spotify_application_client_secret = "Client Secret from Spotify Developer Dashboard"
-spotify_application_redirect_uri = "https://localhost/"
+spotify_application_redirect_uri = "http://localhost:3000/"
