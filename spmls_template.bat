ba1a1406674c9e31b754e68a91c1090a870b0a83
wolveraptor <48845193+Wolveraptor@users.noreply.github.com>
2024-07-25 08:27:57 -0400

    Create spmls_template.bat

---
+++ spmls_template.bat
@@ -0,0 +1,4 @@
+spmls\Scripts\activate.bat && ^
+py -m pip install -r requirements.txt && ^
+py spotify_playlist_multi-level_sorting.py <Spotify Client ID> <Spotify Client Secret> && ^
+spmls\Scripts\deactivate.bat
