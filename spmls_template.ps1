c19dae570efbabaf42dfbd7c6171c138b9ff42a1
wolveraptor <48845193+Wolveraptor@users.noreply.github.com>
2024-12-05 08:32:13 -0500

    Update spmls_template.ps1

--- spmls_template.ps1
+++ spmls_template.ps1
@@ -5,3 +5,4 @@ Start-Process -FilePath "python.exe" -ArgumentList "-m pip install -r requiremen
 Start-Process -FilePath "python.exe" -ArgumentList "spotify_playlist_multi-level_sorting.py Spotify_Client_ID Spotify_Client_Secret" -NoNewWindow -Wait
 deactivate
 Remove-Item -Path "spmls" -Recurse -Force
+Remove-Item -Path ".\.cache" -Recurse -Force
