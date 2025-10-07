ac78b0a0b82d73e0abc76656d13da1f44a5d7217
Stephen Simon <ssimon@noreply.forgejo.cybersanctuary.xyz>
2025-10-07 17:01:40 -0400

    Update "spmls_template_python312.bat".

--- spmls_template_python312.bat
+++ spmls_template_python312.bat
@@ -1,8 +1,8 @@
-C:\Users\ssimon\AppData\Local\Programs\Python\Python312\python.exe -m venv spmls && ^
+C:\Users\%Username%\AppData\Local\Programs\Python\Python312\python.exe -m venv spmls && ^
 spmls\Scripts\activate.bat && ^
-C:\Users\ssimon\AppData\Local\Programs\Python\Python312\python.exe -m pip install --upgrade pip && ^
-C:\Users\ssimon\AppData\Local\Programs\Python\Python312\python.exe -m pip install -r requirements.txt && ^
-C:\Users\ssimon\AppData\Local\Programs\Python\Python312\python.exe spotify_playlist_multi-level_sorting.py Spotify_Client_ID Spotify_Client_Secret && ^
+C:\Users\%Username%\AppData\Local\Programs\Python\Python312\python.exe -m pip install --upgrade pip && ^
+C:\Users\%Username%\AppData\Local\Programs\Python\Python312\python.exe -m pip install -r requirements.txt && ^
+C:\Users\%Username%\AppData\Local\Programs\Python\Python312\python.exe spotify_playlist_multi-level_sorting.py Spotify_Client_ID Spotify_Client_Secret && ^
 spmls\Scripts\deactivate.bat && ^
 rmdir spmls /s /q && ^
 rmdir .\.cache /s /q && ^
