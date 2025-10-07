a0d634190f0be6f9d00ba3267179294ab5f16996
Stephen Simon <ssimon@noreply.forgejo.cybersanctuary.xyz>
2025-10-07 16:59:04 -0400

    Upload "spmls_template_python312.ps1".

---
+++ spmls_template_python312.ps1
@@ -0,0 +1,9 @@
+Start-Process -FilePath "C:\Users\ssimon\AppData\Local\Programs\Python\Python312\python.exe" -ArgumentList "-m venv spmls" -NoNewWindow -Wait
+.\spmls\Scripts\Activate.ps1
+Start-Process -FilePath "C:\Users\ssimon\AppData\Local\Programs\Python\Python312\python.exe" -ArgumentList "-m pip install --upgrade pip" -NoNewWindow -Wait
+Start-Process -FilePath "C:\Users\ssimon\AppData\Local\Programs\Python\Python312\python.exe" -ArgumentList "-m pip install -r requirements.txt" -NoNewWindow -Wait
+Start-Process -FilePath "C:\Users\ssimon\AppData\Local\Programs\Python\Python312\python.exe" -ArgumentList "spotify_playlist_multi-level_sorting.py Spotify_Client_ID Spotify_Client_Secret" -NoNewWindow -Wait
+deactivate
+Remove-Item -Path "spmls" -Recurse -Force
+Remove-Item -Path ".\.cache" -Recurse -Force
+Remove-Item -Path ".\Modules\__pycache__" -Recurse -Force
