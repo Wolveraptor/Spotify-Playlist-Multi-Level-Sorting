9e7f5f2cf41095ae14c5a03d30ff67d4ed7b5ae6
Stephen Simon <ssimon@noreply.forgejo.cybersanctuary.xyz>
2025-10-07 16:57:56 -0400

    Delete "spmls_template.ps1".

--- spmls_template.ps1
+++
@@ -1,8 +0,0 @@
-Start-Process -FilePath "python.exe" -ArgumentList "-m venv spmls" -NoNewWindow -Wait
-.\spmls\Scripts\Activate.ps1
-Start-Process -FilePath "python.exe" -ArgumentList "-m pip install --upgrade pip" -NoNewWindow -Wait
-Start-Process -FilePath "python.exe" -ArgumentList "-m pip install -r requirements.txt" -NoNewWindow -Wait
-Start-Process -FilePath "python.exe" -ArgumentList "spotify_playlist_multi-level_sorting.py Spotify_Client_ID Spotify_Client_Secret" -NoNewWindow -Wait
-deactivate
-Remove-Item -Path "spmls" -Recurse -Force
-Remove-Item -Path ".\.cache" -Recurse -Force
